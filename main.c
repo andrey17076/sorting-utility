0837b7ed8ca438883910a3af1eec46641451c4da
Andrey Tatarenko <andrey17076@gmail.com>
2016-03-08 18:00:12 +0300

    remove some warnings

--- main.c
+++ main.c
@@ -11,12 +11,14 @@ int copyfile(const char *sourse_file, const char *dist_file) {
     int sourse_f, dist_f;
     ssize_t bytes_number;
     char buffer[BUFSIZE];
-    if ((sourse_f = open(sourse_f, O_RDONLY)) == -1)
+
+    if ((sourse_f = open(sourse_file, O_RDONLY)) == -1)
         return (-1);
-    if ((dist_f = open(dist_file, O_WRONLY|O_CREAT|O_TRUNC, 0777) == -1) {
+    if ((dist_f = open(dist_file, O_WRONLY|O_CREAT|O_TRUNC, 0777) == -1)) {
         close(sourse_f);
         return (-2);
     }
+
     while ((bytes_number = read(sourse_f, buffer, BUFSIZE)) > 0) {
         if (write(dist_f, buffer, bytes_number) < bytes_number) {
             close(sourse_f);
@@ -24,6 +26,7 @@ int copyfile(const char *sourse_file, const char *dist_file) {
             return (-3);
         }
     }
+
     close(sourse_f);
     close(dist_f);
 
